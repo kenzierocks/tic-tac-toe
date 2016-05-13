73b6542ae0f253e788921202e050c241da0046d3
Kenzie Togami <ket1999@gmail.com>
2016-05-13 12:22:52 -0700

    More network changes.

--- src/main/java/me/kenzierocks/ttt/packets/NetworkManager.java
+++ src/main/java/me/kenzierocks/ttt/packets/NetworkManager.java
@@ -3,17 +3,15 @@ package me.kenzierocks.ttt.packets;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
-import java.net.Socket;
 import java.nio.ByteBuffer;
-import java.util.Iterator;
+import java.util.Optional;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class NetworkManager implements Runnable {
+public class NetworkManager {
 
     private static final int LENGTH_OF_LENGTH_PACKET = Integer.BYTES;
     private static final ThreadLocal<DatagramPacket> LENGTH_PACKET =
@@ -31,30 +29,40 @@ public class NetworkManager implements Runnable {
         this.socket.joinGroup(mcAddress);
     }
 
-    @Override
-    public void run() {
+    public Runnable getReadRunnable() {
+        return () -> {
+            try {
+                while (!this.socket.isClosed()) {
+                    readQueue.add(readPacket());
+                }
+            } catch (IOException e) {
+                // TODO report to user
+                e.printStackTrace();
+            }
+        };
+    };
+
+    private Packet readPacket() throws IOException {
         // Datagram packets are weird...
         // We'll read a length, then read the packet based on the length
-        try {
-            while (!this.socket.isClosed()) {
-                DatagramPacket lengthPacket = LENGTH_PACKET.get();
-                this.socket.receive(lengthPacket);
-                ByteBuffer data = ByteBuffer.wrap(lengthPacket.getData());
-                int length = data.getInt(0);
-                DatagramPacket realPacket =
-                        new DatagramPacket(new byte[length], length);
-                // First 4 bytes are the ID
-                data = ByteBuffer.wrap(realPacket.getData());
-                int id = data.getInt(0);
-                PacketReader<?> reader = PacketRegistry.getReaderById(id);
-                Packet read = reader
-                        .read(new DataInputStream(new ByteArrayInputStream(
-                                realPacket.getData(), 4, length - 4)));
-            }
-        } catch (IOException e) {
-            // TODO report to user
-            e.printStackTrace();
-        }
+        DatagramPacket lengthPacket = LENGTH_PACKET.get();
+        this.socket.receive(lengthPacket);
+        ByteBuffer data = ByteBuffer.wrap(lengthPacket.getData());
+        int length = data.getInt(0);
+        // TODO length checks?
+        DatagramPacket realPacket =
+                new DatagramPacket(new byte[length], length);
+        // First 4 bytes are the ID
+        data = ByteBuffer.wrap(realPacket.getData());
+        int id = data.getInt(0);
+        PacketReader<?> reader = PacketRegistry.getReaderById(id);
+        Packet read = reader.read(new DataInputStream(
+                new ByteArrayInputStream(realPacket.getData(), 4, length - 4)));
+        return read;
+    }
+
+    public Optional<Packet> getNextPacket() {
+        return Optional.of(this.readQueue.poll());
     }
 
 }
